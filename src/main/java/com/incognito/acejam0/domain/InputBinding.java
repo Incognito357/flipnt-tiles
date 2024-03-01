90df2c0ef7b959f274ce91ebcfe487c535adae1a
Incognito357 <jrh.JackSparrow042@yahoo.com>
2024-03-01 18:48:38 -0500

    Added player movement

---
+++ src/main/java/com/incognito/acejam0/domain/InputBinding.java
@@ -0,0 +1,8 @@
+package com.incognito.acejam0.domain;
+
+public enum InputBinding {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
--- src/main/java/com/incognito/acejam0/states/PlayerState.java
+++ src/main/java/com/incognito/acejam0/states/PlayerState.java
@@ -1,13 +1,21 @@
 package com.incognito.acejam0.states;
 
 import com.incognito.acejam0.Application;
+import com.incognito.acejam0.domain.InputBinding;
 import com.incognito.acejam0.domain.Level;
 import com.incognito.acejam0.domain.Tile;
+import com.jme3.input.InputManager;
+import com.jme3.input.JoyInput;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.JoyAxisTrigger;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.material.Materials;
 import com.jme3.material.RenderState;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
+import com.jme3.math.Vector2f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
@@ -17,6 +25,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class PlayerState extends TypedBaseAppState<Application> {
 
@@ -24,9 +33,60 @@ public class PlayerState extends TypedBaseAppState<Application> {
 
     private Level level;
     private Node rootNode;
-    private Node playersNode = new Node();
-    private List<Spatial> players = new ArrayList<>();
+    private final Node playersNode = new Node();
+    private final List<Map.Entry<Spatial, Vector2f>> players = new ArrayList<>();
     private Material mat;
+
+    private InputManager inputManager;
+    private final Map<InputBinding, ActionListener> listeners = Map.of(
+            InputBinding.UP, (name, isPressed, tpf) -> {
+                if (isPressed) {
+                    move(0, -1);
+                }
+            },
+            InputBinding.DOWN, (name, isPressed, tpf) -> {
+                if (isPressed) {
+                    move(0, 1);
+                }
+            },
+            InputBinding.LEFT, (name, isPressed, tpf) -> {
+                if (isPressed) {
+                    move(-1, 0);
+                }
+            },
+            InputBinding.RIGHT, (name, isPressed, tpf) -> {
+                if (isPressed) {
+                    move(1, 0);
+                }
+            });
+
+    private void move(int dx, int dy) {
+        for (Map.Entry<Spatial, Vector2f> kvp : players) {
+            Vector2f p = kvp.getValue();
+            int x = (int) p.x;
+            int y = (int) p.y;
+
+            int nx = x + dx;
+            int ny = y + dy;
+
+            if (nx < 0 || ny < 0 || nx >= level.getWidth() || ny >= level.getHeight()) {
+                return;
+            }
+
+            if (!level.isTileEnabled(nx, ny)) {
+                return;
+            }
+
+            Tile tile = level.getTile(nx, ny);
+            if (tile == Tile.WALL || tile == Tile.EMPTY) {
+                return;
+            }
+
+            p.x = nx;
+            p.y = ny;
+            kvp.getKey().setLocalTranslation(kvp.getKey().getLocalTranslation().add(dx, -dy, 0));
+        }
+    }
 
     public PlayerState(Level level) {
         this.level = level;
@@ -35,6 +95,7 @@ public class PlayerState extends TypedBaseAppState<Application> {
     @Override
     protected void onInitialize(Application app) {
         rootNode = app.getRootNode();
+        inputManager = app.getInputManager();
 
         mat = new Material(app.getAssetManager(), Materials.UNSHADED);
         mat.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
@@ -50,18 +111,52 @@ public class PlayerState extends TypedBaseAppState<Application> {
                 g.setMaterial(mat);
                 g.rotate(FastMath.HALF_PI, 0, 0);
                 g.setLocalTranslation(x + 0.5f, -y - 0.5f, 0);
-                players.add(g);
+                players.add(Map.entry(g, new Vector2f(x, y)));
                 playersNode.attachChild(g);
             }
         }
         playersNode.setLocalTranslation(-level.getWidth() / 2.0f, level.getHeight() / 2.0f, 0);
 
         rootNode.attachChild(playersNode);
+
+        inputManager.addMapping(InputBinding.UP.name(),
+                new KeyTrigger(KeyInput.KEY_W),
+                new KeyTrigger(KeyInput.KEY_UP),
+                new JoyAxisTrigger(0, JoyInput.AXIS_POV_Y, true));
+        inputManager.addMapping(InputBinding.DOWN.name(),
+                new KeyTrigger(KeyInput.KEY_S),
+                new KeyTrigger(KeyInput.KEY_DOWN),
+                new JoyAxisTrigger(0, JoyInput.AXIS_POV_Y, false));
+        inputManager.addMapping(InputBinding.LEFT.name(),
+                new KeyTrigger(KeyInput.KEY_A),
+                new KeyTrigger(KeyInput.KEY_LEFT),
+                new JoyAxisTrigger(0, JoyInput.AXIS_POV_X, false));
+        inputManager.addMapping(InputBinding.RIGHT.name(),
+                new KeyTrigger(KeyInput.KEY_D),
+                new KeyTrigger(KeyInput.KEY_RIGHT),
+                new JoyAxisTrigger(0, JoyInput.AXIS_POV_X, true));
+
+        addListener(inputManager, InputBinding.UP);
+        addListener(inputManager, InputBinding.DOWN);
+        addListener(inputManager, InputBinding.LEFT);
+        addListener(inputManager, InputBinding.RIGHT);
+    }
+
+    private void addListener(InputManager inputManager, InputBinding mapping) {
+        inputManager.addListener(listeners.get(mapping), mapping.name());
+    }
+
+    private void removeListener(InputManager inputManager, InputBinding mapping) {
+        inputManager.removeListener(listeners.get(mapping));
     }
 
     @Override
     protected void onCleanup(Application app) {
         rootNode.detachChild(playersNode);
+        removeListener(inputManager, InputBinding.UP);
+        removeListener(inputManager, InputBinding.DOWN);
+        removeListener(inputManager, InputBinding.LEFT);
+        removeListener(inputManager, InputBinding.RIGHT);
     }
 
     public void setLevel(Level level) {
